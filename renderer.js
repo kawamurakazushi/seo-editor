3bd726bf14226942120e305c701fd6affe55c07e
Kazushi Kawamura <wakaka2277@gmail.com>
2018-06-24 14:55:00 +0900

    Add keyword highlight

--- renderer.js
+++ renderer.js
@@ -6,7 +6,8 @@ var app = new Vue({
         value: "キーワード"
       }
     ],
-    body: "ここに本文を入力"
+    body: "ここに本文を入力",
+    highlight: "ここに本文を入力"
   },
   methods: {
     addKeyword: function() {
@@ -16,6 +17,25 @@ var app = new Vue({
     },
     removeKeyword: function(i) {
       this.keywords.splice(i, 1);
+      this.applyHighlight();
+    },
+    scroll: function(e) {
+      var top = this.$el.querySelector(".editable").scrollTop;
+      this.$el.querySelector(".backdrop").scrollTo(0, top);
+      console.log(top);
+      console.log("scrolll", this.$el);
+    },
+    change: function() {
+      this.applyHighlight();
+    },
+    applyHighlight: function() {
+      var re = new RegExp(
+        this.keywords.map(keyword => keyword.value).join("|"),
+        "g"
+      );
+      this.highlight =
+        this.body.replace(re, "<span class='mark'>$&</span>") +
+        "<p style='color: transparent'>-----</p>";
     }
   }
 });
